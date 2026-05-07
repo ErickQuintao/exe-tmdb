4b46b9dd4c3e15fcd9e4bc43483ccd7fa529bdf7
erick <erickquintao0@gmail.com>
2026-05-07 20:47:44 -0300

    parte 1 show img e part 2 show genrs ok

--- script.js
+++ script.js
@@ -1,36 +1,67 @@
 const TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNGI2MDRhOGRkMmRkOWY5OGIwNDdkYzRmNWRlYTMwNyIsIm5iZiI6MTc3NzU1OTUzMS41NjQ5OTk4LCJzdWIiOiI2OWYzNjdlYjZkNDY1NTZkOGViNzIxN2YiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.7npxWyEU6UXnx4APUvFFLrAUtpMmatya1bbelWCNzKY";
+
 const opcoes = {
     method: "GET",
     headers: {
-        accept: "aplication/json",
+        accept: "application/json",
         AUTHORIZATION: `Bearer ${TOKEN}`
     }
-}
+};
 
 const URL_FILMES = "https://api.themoviedb.org/3/movie/now_playing";
-const mensagem = document.getElementById("mensagem");
-const listaFilmes = document.getElementById("listaFilmes")
-function iniciarPagina(){
-    console.log("iniciar");
-    buscarFilmes();
+const URL_GENEROS = "https://api.themoviedb.org/3/genre/movie/list?language=pt-BR";
+const listaFilmes = document.getElementById("listaFilmes");
+
+async function iniciarPagina() {
+    const generos = await buscarGenero();
+    await buscarFilmes(generos);
 }
 
-
-
-async function buscarFilmes(){
-    const resposta = await fetch(URL_FILMES,opcoes);
+async function buscarGenero() {
+    const resposta = await fetch(URL_GENEROS, opcoes);
     const dados = await resposta.json();
-    return mostrarFilmes(dados.results);
+    return dados.genres;
 }
-function mostrarFilmes(filmes){
-    for(let i = 0;i< filmes.length;i++){
-        filme = filmes[i];
-        listaFilmes.innerHTML+=`
+
+function mostrarGenero(filme, generos) {
+    const nomesDosGeneros = filme.genre_ids.map(function(idGenero) {
+        const generoEncontrado = generos.find(function(genero) {
+            return genero.id === idGenero;
+        });
+
+        if (generoEncontrado) {
+            return generoEncontrado.name;
+        }
+
+        return "Gênero não encontrado";
+    });
+
+    return nomesDosGeneros.join(", ");
+}
+
+async function buscarFilmes(generos) {
+    const resposta = await fetch(URL_FILMES, opcoes);
+    const dados = await resposta.json();
+    mostrarFilmes(dados.results, generos);
+}
+
+function mostrarFilmes(filmes, generos) {
+    listaFilmes.innerHTML = "";
+
+    for (let i = 0; i < filmes.length; i++) {
+        const filme = filmes[i];
+        const img = `https://image.tmdb.org/t/p/w185/${filme.poster_path}`;
+        const textoGeneros = mostrarGenero(filme, generos);
+
+        listaFilmes.innerHTML += `
             <div class="filme">
-                <p>${filme.title}</p>
+                <img src="${img}" alt="Pôster do filme ${filme.title}">
+                <p id="title"><strong>${filme.title}</strong></p>
+                <p><strong>Gêneros:</strong> ${textoGeneros}</p>
                 <p>${filme.overview}</p>
             </div>
         `;
     }
 }
+
 iniciarPagina();
